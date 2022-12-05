cbfd194a56156463c724c3b804673f0a36791b67
Golikov Ivan <60191541+TheSaintDeer@users.noreply.github.com>
2022-12-05 21:24:57 +0100

    .

--- main.cpp
+++ main.cpp
@@ -9,36 +9,6 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-
-
-//    PointN direction(4);
-//    direction.x[0] = stod(argv[1]);
-//    direction.x[1] = stod(argv[2]);
-//    direction.x[2] = stod(argv[3]);
-//    direction.x[3] = stod(argv[4]);
-//
-//    double cD = stod(argv[5]);
-//
-//    Wind wind;
-//    wind.direction.x[0] = stod(argv[6]);
-//    wind.direction.x[1] = stod(argv[7]);
-//    wind.direction.x[2] = stod(argv[8]);
-//    wind.force = stod(argv[9]);
-//-------------------------------------------------------------------------------
-//    PointN direction(4);
-//    direction.x[0] = 1;
-//    direction.x[1] = 0;
-//    direction.x[2] = (45.0/180.0) * M_PI;
-//    direction.x[3] = 70;
-//
-//    double cD = 0.5;
-//
-//    PointN dir = PointN(3);
-//    dir.x[0] = 0;
-//    dir.x[1] = 0;
-//    dir.x[2] = 0;
-//    Wind wind(dir, 0);
-// ------------------------------------------------------------------------------
     PointN direction(4);
     double cD = 0.0;
     double wForce = 0.0;
